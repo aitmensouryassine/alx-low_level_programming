16dad07ba9ec886fa614fc10337abd6d9abcf673
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-17 08:14:29 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int alphabet_length = 26;
+	char first_letter = 'a';
+	int i = 0;
+
+	while (i < alphabet_length)
+	{
+		if (first_letter + i == 'q' || first_letter + i == 'e')
+		{
+			continue;
+		}
+		putchar(first_letter + i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
