d6199e5bf4a98cd1cf155506e50a78fcc23049a1
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-17 08:04:34 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int alphabet_length = 26;
+	char first_letter = 'a';
+	int i = 0;
+
+	while (i < alphabet_length)
+	{
+		putchar(first_letter + i);
+		i++;
+	}
+
+	return (0);
+}
