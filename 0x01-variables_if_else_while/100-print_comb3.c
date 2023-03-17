187a319627868dec440bcc87f383e721a42e86ab
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-17 09:56:41 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+*/
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i == j || j <= i)
+				continue;
+
+			putchar('0' + i);
+			putchar('0' + j);
+
+			if (i >= 8)
+				break;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+}
