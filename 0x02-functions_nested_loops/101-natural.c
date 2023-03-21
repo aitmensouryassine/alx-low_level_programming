e058ff3e6a4a9777379f78c2fdc5c000f994de4d
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-21 16:05:28 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -3,30 +3,40 @@
 /**
  * print_sum_of_multiples_of_3_and_5 - computes and prints the
  * sum of all the multiples of 3 or 5 below 1024
- *
+ * @n: number below which we calculate the sum of multiples of 3 and 5
  * Return: nothing
  */
-void print_sum_of_multiples_of_3_and_5(void)
+void print_sum_of_multiples_of_3_and_5(int n)
 {
-	int i = 1024;
 	int sum = 0;
 
-	i--;
-	while (i >= 0)
+	n--;
+	while (n >= 0)
 	{
-		if (i % 5 == 0)
+		if (n % 5 == 0)
 		{
-			sum += i;
-			i--;
+			sum += n;
+			n--;
 			continue;
 		}
-		if (i % 3 == 0)
+		if (n % 3 == 0)
 		{
-			sum += i;
-			i--;
+			sum += n;
+			n--;
 			continue;
 		}
-		i--;
+		n--;
 	}
 	printf("%d\n", sum);
+}
+/**
+ * main - netry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_sum_of_multiples_of_3_and_5(1024);
+
+	return (0);
 }
