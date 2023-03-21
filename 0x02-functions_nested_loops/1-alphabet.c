f7b4e18bd173c444ca920058c96b5f38fcecfdd4
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-21 04:48:41 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet followed by a new line
+ * Return - nothing
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
