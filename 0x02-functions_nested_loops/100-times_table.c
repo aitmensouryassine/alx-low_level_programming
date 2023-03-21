a4bb9471990e87fed1c2fcc5f81532872b0950fb
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-21 14:59:46 -0700

    Task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+/**
+ * print_long_int - print int > 9 with putchar
+ *
+ * @n: the number to print
+ *
+ * Return: nothing
+ */
+void print_long_int(int n)
+{
+	if (n / 10)
+		print_long_int(n / 10)
+
+	_putchar(n % 10 + '0');
+}
 /**
  * print_times_table - prints the n times table, starting with 0.
  *
@@ -27,7 +40,7 @@ void print_times_table(int n)
 				_putchar(' ');
 			}
 
-			printf("%d", res);
+			print_long_int(res);
 
 			if (j == n)
 			{
