890b94c34e9bad231151a161e266a4ade2683380
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-21 14:26:44 -0700

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: the integer from which to start printing nutural numbers
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	int to = 98;
+
+	if (n < to)
+	{
+		while (n <= to)
+		{
+			printf("%d", n);
+
+			if (n != to)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n++;
+		}
+	} else
+	{
+		while (n >= to)
+		{
+			printf("%d", n);
+
+			if (n != to)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			n--;
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c~
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two number
+ * @a: first integer of the addition operation
+ * @b: second integer of the addition operation
+ * Return: the sum (integer) of the two integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
