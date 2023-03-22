e4d4691f4e7461bbc8164ab6ab22a7fc95c28b6c
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-21 17:07:00 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * sum_even_fibonacci - finds and prints the sum of the even-valued terms
+ * @max: max fibonacci number
+ * Return: nothing
+ */
+void sum_even_fibonacci(int max)
+{
+	unsigned long int fib = 1;
+	unsigned long int first_fib = 0;
+	unsigned long int sec_fib =  1;
+	unsigned long int sum = 0;
+
+	while (fib < max)
+	{
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+		}
+
+		first_fib = sec_fib;
+		sec_fib = fib;
+		fib = first_fib + sec_fib;
+	}
+	putchar('\n');
+}
+/**
+ * main - netry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	sum_even_fibonacci(4000000);
+
+	return (0);
+}
