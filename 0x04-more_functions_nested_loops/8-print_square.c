91304b69b66cd3e2e4a26134694887eeda9ea3af
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-23 15:13:19 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square line in the terminal using '#'
+ * followed by a new line
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	} else
+	{
+		putchar('\n');
+	}
+}
