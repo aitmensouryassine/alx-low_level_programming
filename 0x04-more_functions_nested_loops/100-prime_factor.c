5451ea6778dc656926711bb48162b4d86f0e6c97
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-23 16:06:34 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* main - finds and prints the largest prime factor of the
+* number 612852475143, followed by a new line.
+* Return: Always 0
+*/
+
+int main(void)
+{
+	long long int n, i, max;
+
+	n = 612852475143;
+	i = 2;
+	max = 0;
+
+	while (i <= n)
+	{
+		if (n % i == 0)
+		{
+			max = max >= i ? max : i;
+			n = n / i;
+			i = 2;
+		}
+		i++;
+	}
+	printf("%d", max);
+
+	return (0);
+}
