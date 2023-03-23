6a8e543903457081b93a46f32ec81473b6a9191d
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-23 16:46:18 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,41 +7,43 @@
 
 void print_number(int n)
 {
-	int sign;
-	int x;
-	int k, j;
+	int sign = n >= 0 ? 1 : -1;
+	int x = n * sign;
+	int k, j, l;
 
-	sign = n >= 0 ? 1 : -1;
-	x = n * sign;
+	if (sign == -1)
+		putchar('-');
 
 	if (x <= 9)
 	{
-		if (sign == -1)
-			_putchar('-');
-		_putchar(x + '0');
+		putchar(x + '0');
 	} else if (x > 9 && x < 100)
 	{
-		if (sign == -1)
-			_putchar('-');
-		_putchar(x / 10 + '0');
-		_putchar(x % 10 + '0');
+		putchar(x / 10 + '0');
+		putchar(x % 10 + '0');
 	} else if (x >= 100 && x < 1000)
 	{
-		if (sign == -1)
-			_putchar('-');
 		k = x / 10;
-		_putchar(k / 10 + '0');
-		_putchar(k % 10 + '0');
-		_putchar(x % 10 + '0');
+		putchar(k / 10 + '0');
+		putchar(k % 10 + '0');
+		putchar(x % 10 + '0');
 	} else if (x >= 1000 && x < 10000)
 	{
-		if (sign == -1)
-			_putchar('-');
 		k = x / 10;
 		j = k / 10;
-		_putchar(j / 10 + '0');
-		_putchar(j % 10 + '0');
-		_putchar(k % 10 + '0');
-		_putchar(x % 10 + '0');
+		putchar(j / 10 + '0');
+		putchar(j % 10 + '0');
+		putchar(k % 10 + '0');
+		putchar(x % 10 + '0');
+	} else if (x >= 10000 && x < 100000)
+	{
+		k = x / 10;
+		j = k / 10;
+		l = j / 10;
+		putchar(l / 10 + '0');
+		putchar(l % 10 + '0');
+		putchar(j % 10 + '0');
+		putchar(k % 10 + '0');
+		putchar(x % 10 + '0');
 	}
 }
