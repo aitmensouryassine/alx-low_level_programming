e753f5618554f11e9d393591fc22ef87660c64d9
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-23 17:05:14 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,19 +1,6 @@
 #include "main.h"
 
 /**
-* print_positive_number - prints a positive integer using putchar
-* @n: integer to print
-*/
-void print_positive_number(int n)
-{
-	if (n / 10)
-	{
-		print_positive_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
-}
-
-/**
 * print_number - prints an integer using putchar
 * @n: integer to print
 */
@@ -25,6 +12,11 @@ void print_number(int n)
 	x = n * sign;
 
 	if (sign == -1)
-		_putchar('-');
-	print_positive_number(x);
+		putchar('-');
+
+	if (x / 10)
+	{
+		print_positive_number(x / 10);
+	}
+	putchar(x % 10 + '0');
 }
