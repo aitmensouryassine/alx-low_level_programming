0896f2887a7785c406a5ef8841ce4426a6030abe
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-29 14:59:39 -0700

    Re-check Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *s)
 
 	for (i = 0; i < l; i++)
 		for (j = 0; j < 13; j++)
-			if (s[i - 1] == sep[j])
+			if (s[i - 1] == sep[j] || i == 0)
 				if (s[i] >= 'a' && s[i] <= 'z')
 					s[i] = s[i] - 32;
 
