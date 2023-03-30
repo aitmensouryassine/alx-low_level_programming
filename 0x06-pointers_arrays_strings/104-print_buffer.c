04313240401b707e79c01bf8d2b490ee92c71b7b
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-03-30 16:41:10 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,16 +9,17 @@
  */
 void print_hex_2(char *b, int byte, int size)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < 10; i++)
+	for (index = 0; index < 10; index++)
 	{
-		if ((i + byte) >= size)
+		if ((index + byte) >= size)
 			printf("  ");
-		else
-			printf("%02x", b[i + byte]);
 
-		if ((i % 2) != 0 && i != 0)
+		else
+			printf("%02x", b[index + byte]);
+
+		if ((index % 2) != 0 && index != 0)
 			printf(" ");
 	}
 }
@@ -41,17 +42,18 @@ void print_hex_8(int pos)
  */
 void print_printable_char(char *b, int byte, int size)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < 10; i++)
+	for (index = 0; index < 10; index++)
 	{
-		if ((i + byte) >= size)
+		if ((index + byte) >= size)
 			break;
 
-		else if (b[i + byte] >= 31 && b[i + byte] <= 126)
-			_putchar(b[i + byte]);
+		else if (b[index + byte] >= 31 && b[index + byte] <= 126)
+			printf("%c", b[index + byte]);
+
 		else
-			_putchar('.');
+			printf(".");
 	}
 }
 
@@ -63,24 +65,20 @@ void print_printable_char(char *b, int byte, int size)
  */
 void print_buffer(char *b, int size)
 {
-	if (size <= 0)
+	int byte;
+
+	for (byte = 0; byte < size; byte += 10)
 	{
+		print_hex_8(byte);
+		print_hex_2(b, byte, size);
+		print_printable_char(b, byte, size);
+
+		if (byte >= size)
+			continue;
+
 		printf("\n");
 	}
-	else
-	{
-		int byte;
 
-		for (byte = 0; byte < size; byte += 10)
-		{
-			printf("%08x: ", byte);
-			print_hex_2(b, byte, size);
-			print_printable_char(b, byte, size);
-
-			if (byte >= size)
-				continue;
-
-			_putchar('\n');
-		}
-	}
+	if (size <= 0)
+		printf("\n");
 }
