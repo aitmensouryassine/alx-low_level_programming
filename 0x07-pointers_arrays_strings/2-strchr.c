250783f9f92fc8598b9066c20ddfed41453ac02c
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-03 14:31:53 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: the string
+ * @c: the character to lacote
+ * Return: pointer to the first occurence of the character c
+ * and NULL if not found
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
