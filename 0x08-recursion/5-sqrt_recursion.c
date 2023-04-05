b7b664d03623e884247b5146c963ea86dbbae48b
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-05 11:59:08 -0700

    Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sqrt_recursion - calculates the square root of a number
+ * @n: number
+ * @i: iterator
+ * Return: the natural square root of a number if found, if not
+ * returns -1
+ */
+int sqrt_recursion(int n, int i)
+{
+	if (i == 0)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	sqrt_recursion(n, i - 1);
+}
+
+/**
+ * _sqrt_recursion - calculates the square root of a number
+ * @n: number
+ * Return: the natural square root of a number if found, if not
+ * returns -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	return (sqrt_recursion(n, n - 1));
+}
