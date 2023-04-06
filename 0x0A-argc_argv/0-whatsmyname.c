1a4015a5a4e26a3cc581f58bf289381fad0c8543
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-06 15:42:53 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: arguments count
+ * @argv: arguments vector (array)
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
