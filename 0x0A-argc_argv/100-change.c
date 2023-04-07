9f65c7f1b8c34a33a4c143f718631ae11f00bfa1
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-07 12:38:40 -0700

    Task 5 ADVANCED

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make
+ * change for an amount of money.
+ * @argc: arguments count
+ * @argv: arguments vector (array)
+ * Return: Amount of cents to give back, or
+ * if argc == 1 : return 1
+ */
+
+int main(int argc, char **argv)
+{
+	int cents[] = {25, 10, 5, 2, 1};
+	int i, size = sizeof(cents) / sizeof(int);
+	int amount = atoi(argv[1]), count = 0;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (amount < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	while (amount)
+	{
+		for (i = 0; i <= size; i++)
+		{
+			if (amount % cents[i] == 0)
+			{
+				amount -= cents[i];
+				count++;
+				break;
+			}
+		}
+	}
+
+	printf("%d\n", count);
+	return (0);
+}
