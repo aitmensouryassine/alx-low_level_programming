48889477adb630129b77ce5847aae6c1c175a195
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-13 04:45:32 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,9 +7,10 @@
  * Return: a pointer to the allocated memory on success
  * and 98 on failure
  */
+
 void *malloc_checked(unsigned int b)
 {
-	int *m = malloc(b);
+	void *m = malloc(b);
 
 	if (m == NULL)
 		exit(98);
