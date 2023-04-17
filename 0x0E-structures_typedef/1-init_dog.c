d792b79df57e58106632ed89e65d6fbfafe73fd6
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-17 07:57:47 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+/**
+ * init_dog - intializes a variable of type struct dog
+ * @d: a pointer to the variable
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the owner
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
