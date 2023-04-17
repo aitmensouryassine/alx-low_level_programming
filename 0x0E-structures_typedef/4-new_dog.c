f25b57ad9f5cdb25f5d83f92372471e88f418f01
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-17 09:26:03 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,8 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
 	if (d->owner == NULL)
 	{
-		free(d);
 		free(d->name);
+		free(d);
 		return (NULL);
 	}
 
