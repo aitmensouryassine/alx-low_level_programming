1ce07469288b4d9b540ffc8892641c455091e534
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-17 09:13:04 -0700

    Task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees dogs
+ * @d: dog structure to free
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+	free(d->name);
+	free(d->owner);
+}
