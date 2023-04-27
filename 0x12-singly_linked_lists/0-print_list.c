7d3bf8564b3add1a33bb54fe91cd15d1ca1246be
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-27 04:28:23 -0700

    Task 0: fix null str

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,13 +8,19 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
+	char *str;
+	int len = 0;
 
 	while (h)
 	{
-		printf("[%i] %s\n", h->len, h->str);
+		str = h->str, len = h->len;
+		if (!str)
+			str = "(nil)", len = 0;
+
+		printf("[%i] %s\n", len, str);
 		h = h->next;
 		n++;
 	}
 
-	return n;
+	return (n);
 }
