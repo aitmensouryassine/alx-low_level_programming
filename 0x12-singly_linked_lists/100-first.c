1722205fa48640064d489568fcb56cc32087d6bf
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-27 13:08:58 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,17 +3,6 @@
 void before_main(void) __attribute__((constructor));
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
-
-/**
  * before_main - executes before main
  */
 void before_main(void)
