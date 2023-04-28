5bc7c7bf3a5c8942feb067f4de6bad5c341a4cfe
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-28 07:18:25 -0700

    Task 0 & 1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h: a pointer to the head of the list
+ * Return: return the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -15,5 +18,7 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
