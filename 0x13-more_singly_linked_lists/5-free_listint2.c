212c2c82bcb40b08d5efad79d13a1d313787eeba
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-04-29 06:17:57 -0700

    Task5: fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (!head)
-		return;
-
-	while ((*head))
+	if (head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		while ((*head))
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
+		*head = NULL;
 	}
-
-	*head = NULL;
 }
