70b9733259ec8172c291db36046b06cf2c309d59
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-05-01 16:08:53 -0700

    Task 14

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,28 +1,6 @@
 #include "lists.h"
 
 /**
- * is_node_checked1 - checks if a node already checked
- * @head: head of the list
- * @curr: the current node
- * @nodes: number of checked nodes
- * Return: 1 if checked, and 0 when not
- */
-int is_node_checked1(listint_t *head, listint_t *curr, size_t nodes)
-{
-	size_t i = 0;
-
-	while (i < nodes)
-	{
-		if (head == curr)
-			return (1);
-		head = (head)->next;
-		i++;
-	}
-
-	return (0);
-}
-
-/**
  * find_listint_loop - finds the loop in a linked list.
  * @head: the head of the list
  * Return: The address of the node where the loop starts,
@@ -30,20 +8,31 @@ int is_node_checked1(listint_t *head, listint_t *curr, size_t nodes)
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	size_t nodes = 0;
-	listint_t *curr = head;
+	listint_t *s = head, *f = head;
 
 	if (!head)
 		return (NULL);
 
-	while (curr)
+	while (s && f && f->next)
 	{
-		curr = curr->next;
-		nodes++;
+		s = s->next;
+		f = f->next->next;
 
-		if (is_node_checked1(head, curr, nodes))
+		if (s == f)
 		{
-			return (curr);
+			break;
+		}
+	}
+
+	s = head;
+	while (s != f && s && f)
+	{
+		s = s->next;
+		f = f->next;
+
+		if (s == f)
+		{
+			return (s);
 		}
 	}
 
