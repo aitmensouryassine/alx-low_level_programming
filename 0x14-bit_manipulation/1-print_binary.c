be4e4d7862164a31be481658a8e7a67b9824c3e7
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-05-04 07:07:12 -0700

    Task 0

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: the number to convert to binary
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int tmp = n;
+	int div_count = 0;
+
+	while (tmp >> 1)
+	{
+		tmp = tmp >> 1; /* tmp = tmp / 2 */
+		div_count++;
+	}
+
+	if (!div_count)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (div_count)
+	{
+		/* n % div_count == 0 */
+		if (((n >> div_count) & 1) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		div_count--;
+	}
+}
