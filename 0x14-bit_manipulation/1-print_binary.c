e69b349b4ca971b6ac09e06460d25a35c8f1147a
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-05-04 07:10:17 -0700

    Task 1 fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 	unsigned long int tmp = n;
 	int div_count = 0;
 
-	while (tmp >> 1)
+	while (tmp)
 	{
 		tmp = tmp >> 1; /* tmp = tmp / 2 */
 		div_count++;
@@ -21,13 +21,12 @@ void print_binary(unsigned long int n)
 		return;
 	}
 
-	while (div_count)
+	while (div_count--)
 	{
 		/* n % div_count == 0 */
 		if (((n >> div_count) & 1) == 0)
 			_putchar('0');
 		else
 			_putchar('1');
-		div_count--;
 	}
 }
