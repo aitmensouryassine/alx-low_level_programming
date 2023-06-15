2a8169dc359999055ff1376ff33ad190d4f8cfbd
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-15 04:14:21 -0700

    Task 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * dlistint_len - prints all the elements of a dlistint_t list.
+ * @h: the head of the list
+ * Return: the size of the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		h = h->next;
+		size++;
+	}
+
+	return (size);
+}
