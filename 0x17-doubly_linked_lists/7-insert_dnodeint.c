796b88b053b6eb7895949848118a5150796da178
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-15 12:46:41 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	node->n = n;
 
-	if (!head && idx = 0)
+	if (!head && idx == 0)
 	{
 		head = node;
 		node->next = NULL;
