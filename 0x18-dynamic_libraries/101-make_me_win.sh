87196293da6d7980365bbca1f8a334222402bbbc
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-06-19 04:09:48 -0700

    Task 32

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp https://github.com/aitmensouryassine/alx-low_level_programming/raw/master/0x18-dynamic_libraries/rand.so
-LD_PRELOAD=/tmp/rand.so
+export LD_PRELOAD=/tmp/rand.so
