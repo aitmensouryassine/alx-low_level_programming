26de5ba80f9d2da0b71eb023816ab22befb27060
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-08-23 18:13:37 +0000

    Task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,15 +7,12 @@ def island_perimeter(grid):
     perim = 0
 
     for row in range(len(grid)):
-        for col in range(len(grid)):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if grid[row][col - 1] == 0:
-                    perim += 1
-                if grid[row][col + 1] == 0:
-                    perim += 1
-                if grid[row - 1][col] == 0:
-                    perim += 1
-                if grid[row + 1][col] == 0:
-                    perim += 1
+                perim += 4
+                if row < len(grid) - 1 and grid[row + 1][col] == 1:
+                    perim -= 2
+                if col < len(grid[row]) - 1 and grid[row][col + 1] == 1:
+                    perim -= 2
 
     return perim
