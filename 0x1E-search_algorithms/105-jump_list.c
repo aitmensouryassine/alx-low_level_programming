9f2f72f81502ad962265c7f55ac3a3693bbfb929
Yassine Ait Mensour <aitmensouryassine@outlook.com>
2023-10-11 10:42:07 +0000

    Task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,6 +1,20 @@
 #include "search_algos.h"
 #include <math.h>
 
+
+listint_t *get_node_by_idx(listint_t *list, size_t size, size_t idx)
+{
+	size_t i;
+
+	if (idx >= size)
+		return (NULL);
+
+	for (i = 0; i < idx; i++)
+		list = list->next;
+
+	return (list);
+}
+
 /**
  * jump_list - searches for a value in a sorted list of integers using
  * the Jump search algorithm
@@ -12,6 +26,43 @@
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
+	listint_t *node;
+	size_t jump, i = 0, do_jump = 1;
+
 	if (!list)
 		return (NULL);
+
+	jump = sqrt(size);
+
+	while (i < size)
+	{
+		node = get_node_by_idx(list, size, i);
+		printf("Value checked at index [%ld] = [%d]\n", i, node->n);
+		if (node->n == value)
+			return (node);
+
+		if (i + jump < size && do_jump)
+		{
+			if (value <= get_node_by_idx(list, size, i + jump)->n)
+			{
+				printf("Value found between indexes [%ld] and [%ld]\n", i, i + jump);
+				do_jump = 0;
+				i -= 1;
+			}
+		}
+
+		if (i + jump >= size && do_jump)
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", i, i + jump);
+			do_jump = 0;
+			i -= 1;
+		}
+
+		if (do_jump)
+			i += jump;
+		else
+			i += 1;
+	}
+
+	return (NULL);
 }
